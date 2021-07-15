7ef463a8f2907e9b271ac773c9a82210c3043cde
IzzyBizzyCoding <87436915+IzzyBizzyCoding@users.noreply.github.com>
2021-07-15 14:23:01 -0400

    QuizzBizz Final Version

---
+++ quizzBizz/quizzBizz/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  quizzBizz
+//
+//  Created by  Scholar on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func startNow(_ sender: Any) {
+    }
+    
+}
+
